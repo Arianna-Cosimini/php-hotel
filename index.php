b61c8a295c1e5f521d3c5b29448ce1d32b58446d
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-18 18:25:10 +0100

    esercizio finito

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $hotels = [
 
 $parking = isset($_GET['parking']);
 $bestHotel = isset($_GET['bestHotel']);
-$index = 1 ; // Variabile per contare il numero di righe
+$index = 1; // Variabile per contare il numero di righe
 
 
 ?>
@@ -64,7 +64,7 @@ $index = 1 ; // Variabile per contare il numero di righe
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./css/style.css">
 
     <title>Hotels</title>
 </head>
@@ -72,26 +72,31 @@ $index = 1 ; // Variabile per contare il numero di righe
 <body>
 
 
-    <div class="container p-5">
+    <div class="container p-5 rounded">
 
-        <form action="index.php" method="GET" class="d-flex flex-column justify-content-center align-items-center">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="true" id="parking" name="parking">
-                <label class="form-check-label" for="parking">
-                    Hotel con parcheggio
-                </label>
+        <h1 class="display-1 fw-bold text-center mb-5 text-white">Scegli l'hotel che fa per te</h1>
+
+        <form action="index.php" method="GET" class="d-flex flex-column align-items-center">
+            <div class="d-flex gap-4 mb-5">
+
+                <div class="form-check text-white">
+                    <input class="form-check-input" type="checkbox" value="true" id="parking" name="parking">
+                    <label class="form-check-label" for="parking">
+                        Hotel con parcheggio
+                    </label>
+                </div>
+                <div class="form-check text-white">
+                    <input class="form-check-input" type="checkbox" value="true" id="bestHotel" name="bestHotel" <?php if ($bestHotel) echo 'checked'; ?>>
+                    <label class="form-check-label" for="bestHotel">
+                        Miglior punteggio
+                    </label>
+                </div>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="true" id="bestHotel" name="bestHotel" <?php if ($bestHotel) echo 'checked'; ?>>
-                <label class="form-check-label" for="bestHotel">
-                    Miglior punteggio
-                </label>
-            </div>
-            <button type="submit" class="btn btn-dark">Filtra</button>
+            <button type="submit" class="btn btn-outline-light mb-5">Filtra</button>
 
         </form>
 
-        <table class="table">
+        <table class="table text-center border-bottom border-top border-end border-start rounded p-3">
             <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -105,8 +110,8 @@ $index = 1 ; // Variabile per contare il numero di righe
             <tbody>
                 <?php
 
-                foreach($hotels as $hotel){
-                    if((!$parking || $hotel['parking']) && (!$bestHotel || $hotel['vote'] >= 3)){
+                foreach ($hotels as $hotel) {
+                    if ((!$parking || $hotel['parking']) && (!$bestHotel || $hotel['vote'] >= 3)) {
 
                         echo "<tr>";
                         echo "<th scope='row'>$index</th>";
